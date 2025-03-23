5a317699aec8d327477be10d993152879ec2dd75
Rakesh kumar N <rakesh.kumar@coveiot.com>
2025-03-23 19:17:31 +0530

    Framework start

---
+++ src/test/java/org/RakiGowda/AppiumFramework/Base.java
@@ -0,0 +1,107 @@
+package org.RakiGowda.AppiumFramework;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.Duration;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import com.google.common.collect.ImmutableMap;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+
+public class Base {
+	
+	public static AndroidDriver d;
+	public AppiumDriverLocalService service;
+	
+	@BeforeClass
+	public void Start() throws InterruptedException, MalformedURLException, URISyntaxException {
+		service = new AppiumServiceBuilder().withAppiumJS(new File("C://Users//KaHa//AppData//Roaming//npm//node_modules//appium//build//lib//main.js"))
+				.withIPAddress("127.0.0.1").usingPort(4723).build();
+		
+//		service.start();
+		UiAutomator2Options options = new UiAutomator2Options();
+//		options.setApp("C:\\Users\\KaHa\\Documents\\01 - Selenium\\Appium\\src\\test\\java\\resources\\ApiDemos-debug.apk");
+		options.setApp("C:\\Users\\KaHa\\Documents\\01 - Selenium\\Appium\\src\\test\\java\\resources\\General-Store.apk");
+		options.setDeviceName("Appium");
+		options.setChromedriverExecutable("C:\\Users\\KaHa\\Documents\\01 - Selenium\\Appium\\src\\test\\java\\resources\\chromedriver.exe");
+		options.setPlatformName("Android");
+		
+		
+		d = new AndroidDriver( new URI("http://127.0.0.1:4723/wd/hub").toURL(), options);
+		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		Thread.sleep(3);
+		
+	}
+	
+	public Double getFormattedAmount(String amount) {
+		
+		Double Price = Double.parseDouble(amount);
+		
+		return Price;
+				
+	}
+	
+	public void scroll(String text) {
+		d.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(text))"));
+	}
+	
+	public void sleep(int a) throws InterruptedException {
+		Thread.sleep(a * 1000);
+	}
+	
+	public void draganddropgestures(WebElement from_element,int toX, int toY) {
+		((JavascriptExecutor) d).executeScript("mobile: dragGesture", ImmutableMap.of(
+			    "elementId", ((RemoteWebElement) from_element).getId(),
+			    "endX", 650,
+			    "endY", 579
+			));
+	}
+	
+	public void longPressAction(WebElement element) {
+		((JavascriptExecutor) d).executeScript("mobile: longClickGesture", ImmutableMap.of(
+			    "elementId", ((RemoteWebElement) element).getId(),"duration",2000
+			));
+	}
+	
+	public void scrollToEnd() {
+		boolean canScrollMore;
+		do {
+		 canScrollMore = (Boolean) ((JavascriptExecutor) d).executeScript("mobile: scrollGesture", ImmutableMap.of(
+			    "left", 100, "top", 100, "width", 200, "height", 200,
+			    "direction", "down",
+			    "percent", 1.0
+			)); 
+		}
+		while(canScrollMore);
+	}
+	
+	public void swipeGesture(WebElement ele,String direction) {
+		((JavascriptExecutor) d).executeScript("mobile: swipeGesture", ImmutableMap.of(
+				"elementId", ((RemoteWebElement) ele).getId(),
+			    "direction", direction,
+			    "percent", 0.75
+			));
+		
+	} 
+	
+	
+	@AfterClass
+	public void stop() {
+		d.quit();
+//		service.stop();
+	}
+	
+
+}
---
+++ src/test/java/org/RakiGowda/AppiumFramework/eCommerce_tc1_Hybrid.java
@@ -0,0 +1,101 @@
+package org.RakiGowda.AppiumFramework;
+
+import static org.testng.Assert.assertEquals;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import com.google.common.collect.ImmutableMap;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+
+public class eCommerce_tc1_Hybrid extends Base {
+
+	
+
+	@Test
+	public void cart_amt() throws InterruptedException {
+		d.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Rakesh Kumar N");
+		d.hideKeyboard();
+		d.findElement(By.xpath("//android.widget.RadioButton[@text=\"Female\"]")).click();	
+		d.findElement(By.id("com.androidsample.generalstore:id/spinnerCountry")).click();
+		d.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"))"));
+		d.findElement(AppiumBy.androidUIAutomator("new UiSelector().text(\"Argentina\")")).click();
+		d.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+		d.findElements(By.xpath("//android.widget.TextView[@text=\"ADD TO CART\"]")).get(0).click();
+		d.findElements(By.xpath("//android.widget.TextView[@text=\"ADD TO CART\"]")).get(0).click();
+		
+		d.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
+		sleep(2);
+		WebDriverWait wait = new WebDriverWait(d,Duration.ofSeconds(10));
+		
+		wait.until(ExpectedConditions.attributeContains(d.findElement(AppiumBy.id("com.androidsample.generalstore:id/toolbar_title")), "text", "Cart"));
+		
+		
+		List<WebElement> productPrizes = d.findElements(By.id("com.androidsample.generalstore:id/productPrice"));
+		Float sum = (float) 0.0;
+		
+		for(int i=0;i<productPrizes.size();i++) {
+			String amountString = productPrizes.get(i).getText();
+			amountString = amountString.substring(1);
+			
+			sum = sum + Float.parseFloat(amountString);
+		}
+		String AppValue = d.findElement(By.id("com.androidsample.generalstore:id/totalAmountLbl")).getText();
+		float ToTalAmt = Float.parseFloat(AppValue.substring(1));
+		System.out.println("Our Value "+sum);
+		System.out.println("App value " +ToTalAmt);
+		
+		assertEquals(sum, ToTalAmt);
+			
+//			d.findElement(By.id("com.androidsample.generalstore:id/btnProceed")).click();		
+			sleep(2);
+			
+			WebElement element = d.findElement(By.id("com.androidsample.generalstore:id/termsButton"));
+			longPressAction(element);
+			
+			sleep(3);
+			d.findElement(By.id("android:id/button1")).click();
+			sleep(1);
+			
+            d.findElement(By.className("android.widget.CheckBox")).click();
+			
+			d.findElement(By.id("com.androidsample.generalstore:id/btnProceed")).click();
+			
+			sleep(5);
+			
+			
+			Set<String> contexthandles = d.getContextHandles();
+
+
+			for(String CH:contexthandles) {
+				System.out.println(CH);
+			}
+			sleep(5);
+			//Not working
+			d.context("WEBVIEW_com.androidsample.generalstore");
+			sleep(3);
+			d.findElement(By.name("q")).sendKeys("Rahul shetty academy");
+			sleep(2);
+			d.findElement(By.name("q")).sendKeys(Keys.ENTER);
+			sleep(3);
+			d.pressKey(new KeyEvent(AndroidKey.BACK));
+			d.context("NATIVE_APP");
+			
+
+				
+			
+		    sleep(5);
+	}
+}
